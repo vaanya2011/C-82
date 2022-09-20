5abd33a745ed6ab4caa3d35e171b8772dc2dded6
vaanya2011 <100355303+vaanya2011@users.noreply.github.com>
2022-09-20 17:44:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var mouseEvent = "empty";
+var last_x, last_y;
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+color="black";
+width=2;
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
+    color= document.getElementById("color").value;
+    width= document.getElementById("width").value;
+    mouseEvent="mousedown";
+
+}
+canvas.addEventListener("mouseup", my_mouseup);
+
+function my_mouseup(e){
+    mouseEvent="mouseup";
+
+}
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e){
+    mouseEvent="mouseleave";
+
+}
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e){
+    current_x=e.clientX-canvas.offsetLeft;
+    current_y=e.clientY-canvas.offsetTop;
+    if(mouseEvent=="mousedown"){
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=width;
+        ctx.moveTo(last_x, last_y);
+        ctx.lineTo(current_x, current_y);
+        ctx.stroke();
+
+    }
+    last_x=current_x;
+    last_y=current_y;
+
+}
+function c(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+}
+
+
+
+
